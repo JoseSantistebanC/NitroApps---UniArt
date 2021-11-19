deaa328999e9d20f8361c74bbf4c840cc6fd4f18
GonzaloJuradoG <89112800+GonzaloJuradoG@users.noreply.github.com>
2021-11-19 10:32:39 -0500

    Detail de ciudad y pais

--- uniart-app/src/api/apiCiudad.ts
+++ uniart-app/src/api/apiCiudad.ts
@@ -4,15 +4,16 @@ import request from './api';
 
 const apiCiudad = {
 	list: () => request.get<Ciudad[]>("/Ciudad"),
-	add: (data: Ciudad) => request.post("/Ciudad", data),
-	edit: (data: Ciudad) => request.put(`/Ciudad/${data.id}`, data),
-	delete: (id: number) => request.delete(`/Ciudad/${id}`),
-	detail: (id: string) => request.get<Ciudad>(`/Ciudad/${id}`),
+	//add: (data: Ciudad) => request.post("/Ciudad", data),
+	//edit: (data: Ciudad) => request.put(`/Ciudad/${data.id}`, data),
+	//delete: (id: number) => request.delete(`/Ciudad/${id}`),
+	detail: (id: number) => request.get<Ciudad>(`/Ciudad/${id}`),
 };
 
 export default apiCiudad;
 
 
+//READ LIST 
 export const ListCiudades = () => {
   const [ciudades, setCiudades] = React.useState<Ciudad[]>([]);
 	function refreshCiudades(){
@@ -22,4 +23,16 @@ export const ListCiudades = () => {
     });
   }
 	return {ciudades,refreshCiudades};
+};
+
+//READ ONE (DETAILS)
+export const GetCiudad = (id: number) => {
+	const [ciudad, setCiudad] = React.useState<Ciudad>(new Ciudad);
+	function refreshCiudad() {
+		apiCiudad.detail(id).then((res) => {
+			setCiudad(res);
+			console.log('i ciudad:', res);
+		}).catch(() => { "no listó ciudad" });
+	}
+	return { ciudad, refreshCiudad };
 };
